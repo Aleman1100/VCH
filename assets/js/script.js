f188e8c234af61b176f2e5c2910d50ae2d533743
Alejadro Bacquet <alejandro.bacquet@gmail.com>
2021-10-01 13:00:03 -0300

    Preeliminar 1, cambios completos

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Tooltip
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//Descatar al pasar el mouse
+
+function myFunction1() {
+    document.getElementById("destacado1").classList.add("descatadoP");
+  }
+
+function myFunction2() {
+    document.getElementById("destacado2").classList.add("descatadoP");
+  }
+
+function myFunction3() {
+    document.getElementById("destacado3").classList.add("descatadoP");
+  }
+
+  function CargaCompleta() {
+    alert("Gracias por la espera, el formulario carga un poco lento <3");
+  }
